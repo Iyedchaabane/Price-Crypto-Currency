38b0f8001a5dc028cd66e002c280255229b9ea8d
Iyedchaabane <Iyedchaabane@github.com>
2023-10-06 16:30:28 +0100

    F commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CurrencyService } from './services/currency.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PriceCryptoCurrency';
+  selectedCurrency : string = "INR";
+  constructor(private currencyService : CurrencyService){
+
+  }
+  sendCurrency(event:string){
+    console.log(event);
+    this.currencyService.setCurrency(event);
+  }
 }
